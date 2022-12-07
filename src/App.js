44ffc470f3925e6b20e112a91f539344257a9cd8
Joseph Yanong <joeyanong.39+git@gmail.com>
2022-12-07 20:42:20 +0800

    added create-user and deposit function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <UserInput />
+                <Deposit />
+            </header>
+        </div>
+    );
 }
+
+const userList = [];
+
+const UserInput = () => {
+    const [name, setName] = useState("");
+    const [balance, setBalance] = useState("");
+
+    function handleUserInputSubmit(event) {
+        event.preventDefault();
+        userList.push({ username: name, userbalance: balance });
+        console.log("userList", userList);
+        setName("");
+        setBalance("");
+    }
+
+    function handleUserInputChange(event) {
+        setName(event.target.value);
+    }
+
+    function handleBalanceInputChange(event) {
+        setBalance(Number(event.target.value));
+    }
+
+    return (
+        <form className="add-user" onSubmit={handleUserInputSubmit}>
+            <label>
+                Name:
+                <input
+                    type="text"
+                    value={name}
+                    onChange={handleUserInputChange}
+                />
+            </label>
+            <label>
+                Balance:
+                <input
+                    type="text"
+                    value={balance}
+                    onChange={handleBalanceInputChange}
+                />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    );
+};
+
+const Deposit = () => {
+    const [name, setName] = useState("");
+    const [balance, setBalance] = useState("");
+
+    function handleUserInputSubmit(event) {
+        event.preventDefault();
+        userList.push({ username: name, userbalance: balance });
+        console.log("userList", userList);
+        setName("");
+        setBalance("");
+    }
+
+    function handleUserInputChange(event) {
+        setName(event.target.value);
+    }
+
+    function handleBalanceInputChange(event) {
+        setBalance(Number(event.target.value));
+    }
+
+    return (
+        <form className="add-user" onSubmit={handleUserInputSubmit}>
+            <label>
+                Name:
+                <input
+                    type="text"
+                    value={name}
+                    onChange={handleUserInputChange}
+                />
+            </label>
+            <label>
+                Balance:
+                <input
+                    type="text"
+                    value={balance}
+                    onChange={handleBalanceInputChange}
+                />
+            </label>
+            <button type="button">Load User</button>
+            <input type="submit" value="Submit" />
+        </form>
+    );
+};
 
 export default App;
